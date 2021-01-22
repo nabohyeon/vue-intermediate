d91dd88349af4f1d74edf334acf23c90338f9270
nbh <nbh@koino.net>
2021-01-22 17:37:04 +0900

    [1-7강]

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <input type="text" v-model="addTodo">
+      <button v-on:click="addTodoList">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data : function () {
+        return {
+            addTodo : ""
+        }
+    },
+    methods : {
+        addTodoList : function () {
+            localStorage.setItem(this.addTodo, this.addTodo);
+            this.addTodo = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
