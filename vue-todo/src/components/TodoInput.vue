1542ae3925e261dacea03b2c035169c24e890955
nbh <nbh@koino.net>
2021-01-29 10:34:50 +0900

    [TodoList 컴포넌트 UI 스타일링] - 폰트어썸 cdn 링크 추가 - input박스 스타일링 - v-on:keyup.enter 추가 - 폰트어썸 아이콘 사용

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-      <input type="text" v-model="addTodo">
-      <button v-on:click="addTodoList">add</button>
+  <div class="inputBox shadow">
+      <input type="text" v-model="addTodo" v-on:keyup.enter="addTodoList">
+      <!-- <button v-on:click="addTodoList">add</button> -->
+      <span class="addContainer" v-on:click="addTodoList">
+          <i class="fas fa-plus addBtn"></i>
+      </span>
   </div>
 </template>
 
@@ -15,12 +18,38 @@ export default {
     methods : {
         addTodoList : function () {
             localStorage.setItem(this.addTodo, this.addTodo);
+            this.clearInput();
+        },
+        clearInput : function () {
             this.addTodo = "";
         }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    input:focus{
+        outline: none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer{
+        float: right;
+        background: linear-gradient(to right, #6478fb, #8763fb);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn{
+        color: white;
+        vertical-align: middle;
+    }
 </style>
